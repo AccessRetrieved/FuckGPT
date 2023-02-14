67f19a00f07d79110b1b61331c29f26ce7626c96
Jerry Hu <work.jerrywu@gmail.com>
2023-02-13 17:21:37 -0800

    f

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
 
 # Flask app config
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 FLASK_ENV = 'development'
 TESTING = True
-SECRET_KEY = os.environ.get('SECRET_KEY')
 JSON_SORT_KEYS = False
 DEBUG = True
